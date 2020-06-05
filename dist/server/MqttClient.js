7f57d3627c6fa9b873e2ceadefb6195d990eaa4e
Suphawich <suphawich.skv@gmail.com>
2020-06-05 20:09:37 +0700

    Try to disable mqtt client

--- dist/server/MqttClient.js
+++ dist/server/MqttClient.js
@@ -4,8 +4,6 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 
 var _cors = _interopRequireDefault(require("cors"));
 
-var _mqtt = _interopRequireDefault(require("mqtt"));
-
 var _router = _interopRequireDefault(require("../router"));
 
 var _express = _interopRequireDefault(require("express"));
@@ -16,6 +14,7 @@ var _bodyParser = _interopRequireDefault(require("body-parser"));
 
 var _services = require("../app/services");
 
+// import mqtt from "mqtt"
 var app = (0, _express["default"])();
 app.use((0, _cors["default"])());
 app.use(_bodyParser["default"].urlencoded({
@@ -25,26 +24,25 @@ app.use(_bodyParser["default"].json());
 app.use("/", _router["default"]);
 app.listen(_app["default"].SERVER_PORT, function () {
   console.log("Server started and listening on port:", _app["default"].SERVER_PORT);
-}); // Connect MQTT
-
-var client = _mqtt["default"].connect({
-  host: _app["default"].MQTT_SERVER,
-  port: _app["default"].MQTT_PORT,
-  username: _app["default"].MQTT_USER,
-  password: _app["default"].MQTT_PASSWORD
-});
-
-client.on("connect", function () {
-  // Subscribe any topic
-  console.log("MQTT Client connected");
-  client.subscribe("test", function (err) {
-    if (err) {
-      console.log(err);
-    }
-  }); // LineService.pushNoti("U9e5c1fc807fff3db84401a018942b6f1", "Hello, test")
-}); // Receive Message and print on terminal
-
-client.on("message", function (topic, message) {
-  // message is Buffer
-  console.log(topic.toString(), message.toString());
-});
+}); // // Connect MQTT
+// const client = mqtt.connect({
+//   host: config.MQTT_SERVER,
+//   port: config.MQTT_PORT,
+//   username: config.MQTT_USER,
+//   password: config.MQTT_PASSWORD,
+// })
+// client.on("connect", () => {
+//   // Subscribe any topic
+//   console.log("MQTT Client connected")
+//   client.subscribe("test", (err) => {
+//     if (err) {
+//       console.log(err)
+//     }
+//   })
+//   // LineService.pushNoti("U9e5c1fc807fff3db84401a018942b6f1", "Hello, test")
+// })
+// // Receive Message and print on terminal
+// client.on("message", (topic, message) => {
+//   // message is Buffer
+//   console.log(topic.toString(), message.toString())
+// })
--- src/server/MqttClient.js
+++ src/server/MqttClient.js
@@ -1,5 +1,5 @@
 import cors from "cors"
-import mqtt from "mqtt"
+// import mqtt from "mqtt"
 import router from "@router"
 import express from "express"
 import config from "@config/app"
@@ -17,27 +17,27 @@ app.listen(config.SERVER_PORT, () => {
   console.log("Server started and listening on port:", config.SERVER_PORT)
 })
 
-// Connect MQTT
-const client = mqtt.connect({
-  host: config.MQTT_SERVER,
-  port: config.MQTT_PORT,
-  username: config.MQTT_USER,
-  password: config.MQTT_PASSWORD,
-})
+// // Connect MQTT
+// const client = mqtt.connect({
+//   host: config.MQTT_SERVER,
+//   port: config.MQTT_PORT,
+//   username: config.MQTT_USER,
+//   password: config.MQTT_PASSWORD,
+// })
 
-client.on("connect", () => {
-  // Subscribe any topic
-  console.log("MQTT Client connected")
-  client.subscribe("test", (err) => {
-    if (err) {
-      console.log(err)
-    }
-  })
-  // LineService.pushNoti("U9e5c1fc807fff3db84401a018942b6f1", "Hello, test")
-})
+// client.on("connect", () => {
+//   // Subscribe any topic
+//   console.log("MQTT Client connected")
+//   client.subscribe("test", (err) => {
+//     if (err) {
+//       console.log(err)
+//     }
+//   })
+//   // LineService.pushNoti("U9e5c1fc807fff3db84401a018942b6f1", "Hello, test")
+// })
 
-// Receive Message and print on terminal
-client.on("message", (topic, message) => {
-  // message is Buffer
-  console.log(topic.toString(), message.toString())
-})
+// // Receive Message and print on terminal
+// client.on("message", (topic, message) => {
+//   // message is Buffer
+//   console.log(topic.toString(), message.toString())
+// })
